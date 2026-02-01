c6d555c316d3b02c0a81fc0788a0e715980f7a7c
Christina Reilly <christinareilly@Christinas-Mac-mini.local>
2026-02-01 00:37:52 -0500

    add base layout structure for listening section.

--- src/content/now/listening.ts
+++ src/content/now/listening.ts
@@ -1,3 +1,10 @@
 import type { ListeningItem } from "../now.types";
 
-export const listening: ListeningItem[] = [];
+export const listening: ListeningItem[] = [
+     {
+        title: "Body Talk",
+        artist: "Robyn",
+        note: "Trying to sit with this album all week without skipping.",
+        link: "https://open.spotify.com/album/0Rzg7fqyWE39G6wKipxrns",
+    },
+];
