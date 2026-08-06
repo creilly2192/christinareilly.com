49784b35e217d6dd5454629256bd9b5d76fbc1d6
Christina <christina@Christinas-MacBook-Air.local>
2026-08-05 21:33:42 -0400

    update now reading

--- src/data/now/reading.ts
+++ src/data/now/reading.ts
@@ -2,10 +2,32 @@ import type { ReadingItem } from "../now.types";
 
 export const reading: ReadingItem[] = [
   {
+    title: "The Grapes of Wrath",
+    author: "John Steinbeck",
+    status: "current",
+    date: "2026-06-31",
+    year_read: null,
+    note: "A classic novel about the struggles of a family during the Great Depression, highlighting themes of poverty, injustice, and resilience.",
+    cover: "grapes-of-wrath-gr.jpg",
+    goodreadsLink:
+      "https://www.goodreads.com/book/show/18114322-the-grapes-of-wrath",
+  },
+  {
+    title: "Into The Blue",
+    author: "Emma Brodie",
+    status: "finished",
+    date: "2026-05-31",
+    year_read: 2026,
+    note: "An epic, decade-spanning love story that blazes through the worlds of acting and comedy.",
+    cover: "into-the-blue-gr.jpg",
+    goodreadsLink:
+      "https://www.goodreads.com/en/book/show/242765313-into-the-blue",
+  },
+  {
     title: "McTeague",
     author: "Frank Norris",
-    status: "current",
-    date: null,
+    status: "finished",
+    date: "2026-05-04",
     year_read: 2026,
     note: "A dark tale of greed and downfall in San Francisco set in the late 19th century.",
     cover: "mcteague-gr.webp",
@@ -16,9 +38,9 @@ export const reading: ReadingItem[] = [
     title: "Sunrise on the Reaping",
     author: "Suzanne Collins",
     status: "finished",
-    date: "2026-02-85",
+    date: "2026-02-25",
     year_read: 2026,
-    note: "A prequel to The Hunger Games, exploring the origins of the Haymitch.",
+    note: "A prequel to The Hunger Games, exploring the origins of Haymitch.",
     cover: "reaping-gr.webp",
     goodreadsLink:
       "https://www.goodreads.com/book/show/214331246-sunrise-on-the-reaping",
